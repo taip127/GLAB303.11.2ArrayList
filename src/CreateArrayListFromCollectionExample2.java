3109def4c4451aa8184f619d944d612b8b29a8ec
Tai Pham <taipham1192@gmail.com>
2024-12-09 08:41:47 -0600

    up

---
+++ src/CreateArrayListFromCollectionExample2.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Example 3: Accessing Elements from an ArrayList
+This example shows:
+The use of the isEmpty() method to check if an ArrayList is empty.
+The use of the size() method to find the size of an ArrayList.
+The use of the get() method to access an element at a particular index in an ArrayList.
+The use of the set() method to modify the element at a particular index in an ArrayList.
+*/
+public class CreateArrayListFromCollectionExample2 {
+    public static void main(String[] args) {
+        List<String> topCompanies = new ArrayList<>();
+
+        // Check if an ArrayList is empty
+        System.out.println("Is the topCompanies list empty? : " + topCompanies.isEmpty());
+
+        topCompanies.add("Google");
+        topCompanies.add("Apple");
+        topCompanies.add("Microsoft");
+        topCompanies.add("Amazon");
+        topCompanies.add("Facebook");
+
+        // Find the size of an ArrayList
+        System.out.println("Here are the top " + topCompanies.size() + " companies in the world");
+        System.out.println(topCompanies);
+
+        // Retrieve the element at a given index
+        String bestCompany = topCompanies.get(0);
+        String secondBestCompany = topCompanies.get(1);
+        String lastCompany = topCompanies.get(topCompanies.size() - 1);
+
+        System.out.println("Best Company: " + bestCompany);
+        System.out.println("Second Best Company: " + secondBestCompany);
+        System.out.println("Last Company in the list: " + lastCompany);
+
+        // Modify the element at a given index
+        topCompanies.set(4, "Walmart");
+        System.out.println("Modified top companies list: " + topCompanies);
+    }
+}
